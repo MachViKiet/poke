17a064600aacdc32884d23ced9a2772083384712
brunocmg <brunocmg2006@gmail.com>
2025-09-15 19:01:06 -0300

    feat: pokemon logo and dynamic github icon

--- src/components/Theme/Theme.jsx
+++ src/components/Theme/Theme.jsx
@@ -1,17 +1,23 @@
+import React from "react";
 import IconButton from "@mui/material/IconButton";
 import Box from "@mui/material/Box";
-import { useTheme } from "@mui/material/styles";
 
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
 
 const ThemeToggleButton = ({ toggleTheme, themeMode }) => {
-  const theme = useTheme();
-
   return (
-    <IconButton sx={{ ml: 1 }} onClick={toggleTheme} color="inherit">
-      {themeMode === "dark" ? <Brightness7Icon /> : <Brightness4Icon />}
-    </IconButton>
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <IconButton sx={{ ml: 1 }} onClick={toggleTheme} color="inherit">
+        {themeMode === "dark" ? <Brightness7Icon /> : <Brightness4Icon />}
+      </IconButton>
+    </Box>
   );
 };
 
