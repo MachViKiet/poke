7d97dcabaa040d2102771fc9da9b4b4ab7520357
Thiago Sampaio <thiago.sampaiog@gmail.com>
2025-09-23 14:59:34 -0300

    refactor: remove ThemeToggleButton component and simplify Theme component structure

--- src/components/Theme/Theme.jsx
+++ src/components/Theme/Theme.jsx
@@ -1,24 +1,6 @@
-import React from "react";
-import IconButton from "@mui/material/IconButton";
-import Box from "@mui/material/Box";
+const Theme = () => {
 
-import Brightness4Icon from "@mui/icons-material/Brightness4";
-import Brightness7Icon from "@mui/icons-material/Brightness7";
+}
 
-const ThemeToggleButton = ({ toggleTheme, themeMode }) => {
-  return (
-    <Box
-      sx={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <IconButton sx={{ ml: 1 }} onClick={toggleTheme} color="inherit">
-        {themeMode === "dark" ? <Brightness7Icon /> : <Brightness4Icon />}
-      </IconButton>
-    </Box>
-  );
-};
 
-export default ThemeToggleButton;
+export default Theme;
